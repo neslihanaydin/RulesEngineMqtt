77280aed8df68cc38289d5d00d9a1ca7cf4d04f4
Neslihan TURPCU <neslihanaydin43@gmail.com>
2025-01-31 13:20:59 -0800

    Add custom Gson deserializer for case-insensitive FamilyComposition enum parsing, add FamilyComposition field to check in rules.

--- src/main/java/org/neslihantrpc/enums/FamilyComposition.java
+++ src/main/java/org/neslihantrpc/enums/FamilyComposition.java
@@ -3,8 +3,6 @@ package org.neslihantrpc.enums;
 import com.google.gson.annotations.SerializedName;
 
 public enum FamilyComposition {
-    @SerializedName("Single")
     SINGLE,
-    @SerializedName("Married")
     MARRIED;
 }
